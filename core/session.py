4531ae3dc88f784ae31ef458214e641e56bd8729
Manuel Reithuber <git@manuel.reithuber.net>
2016-07-01 11:12:37 +0200

    added core/sock.py (as base for core/session.py as well as others in the future)

--- core/session.py
+++ core/session.py
@@ -1,46 +1,21 @@
 import json
 import modules
+import threading
 import traceback
 import websocket
 
+from core import sock
 from core import service
 
-class Message:
-	def __init__(self, data):
-		assert type(data) == dict
-		self._data = data
-
-	def __contains__(self, key): return key in self._data
-	def __getattr__(self, key): return self._data[key]
-	def __repr__(self): return "Message({0})".format(self._data)
-
-class Session:
+class Session(sock.Socket):
 	""" Connects to the ondevice service """
-	def __init__(self, auth, dev, url="ws://localhost:8080/v0.10/serve/websocket"):
-		# TODO implement proper URL handling
-		self._url = '{0}?auth={1}&id={2}'.format(url, auth, dev)
-		self._ws = websocket.WebSocketApp(self._url,
-			on_message=self._onMessage,
-			on_error=self._onError,
-			on_open=self._onOpen,
-			on_close=self._onClose)
+	def __init__(self, auth, dev):
+		super().__init__('/serve', auth=auth, dev=dev)
 
-	def _onClose(self, ws):
-		print("onClose:")
-		print("  ws={0}".format(ws))
-
-	def _onError(self, ws, error):
-		print("onError:")
-		print("  ws={0}".format(ws))
-		print("  error={0}".format(error))
-
-	def _onMessage(self, ws, messageText):
+	def onMessage(self, msg):
 		try:
-			print("<< {0}".format(messageText))
-			msg = Message(json.loads(messageText))
-
 			if not '_type' in msg:
-				raise Exception("Missing message type: {0}".format(messageText))
+				raise Exception("Missing message type: {0}".format(msg))
 			elif msg._type == 'hello':
 				print("Got hello: "+repr(msg))
 				for name, svc in service.listServices().items():
@@ -52,9 +27,9 @@ class Session:
 				response = {'_type': 'pong', 'ts': msg.ts}
 				self.send(response)
 			elif msg._type == 'connect':
-				print("Got connection request: "+repr(msg))
+				print("Got connection request ({0} active threads): {1}".format(threading.active_count(), repr(msg)))
 				svc = modules.getService(msg)
-				svc.run()
+				t = threading.Thread(target=svc.run)
 
 			else:
 				print("onMessage: unsupported type")
@@ -62,18 +37,6 @@ class Session:
 				print("  msg={0}".format(msg))
 		except Exception as e:
 			# the websocket-client lib swallows the stack traces of exceptions
-			print("Msg: '{0}'".format(messageText))
+			print("Msg: '{0}'".format(msg))
 			traceback.print_exc()
 			raise e
-
-	def _onOpen(self, ws):
-		print("onOpen:")
-		print("  ws={0}".format(ws))
-
-	def run(self):
-		self._ws.run_forever()
-
-	def send(self, msg):
-		data = json.dumps(msg)
-		print('>> {0}'.format(data))
-		self._ws.send(data)
---
+++ core/sock.py
@@ -0,0 +1,50 @@
+import json
+import websocket
+
+class Message:
+    def __init__(self, data):
+        assert type(data) == dict
+        self._data = data
+
+    def __contains__(self, key): return key in self._data
+    def __getattr__(self, key): return self._data[key]
+    def __repr__(self): return "Message({0})".format(self._data)
+
+class Socket:
+    def __init__(self, endpoint, **params):
+        # TODO make base URL configurable
+        # TODO do proper URL handling
+        baseUrl = 'ws://localhost:8080/v0.10'
+        paramStr = '&'.join('{0}={1}'.format(k,v) for k, v in params.items())
+        self._url = '{baseUrl}{endpoint}/websocket?{paramStr}'.format(**locals())
+        self._ws = websocket.WebSocketApp(self._url,
+            on_message=self._onMessage,
+            on_error=self._onError,
+            on_open=self._onOpen,
+            on_close=self._onClose)
+
+    def _onMessage(self, ws, messageText):
+        msg = Message(json.loads(messageText))
+        print("<< {0}".format(messageText))
+        self.onMessage(msg)
+
+    def _onClose(self, ws):
+        print("onClose:")
+        print("  ws={0}".format(ws))
+
+    def _onError(self, ws, error):
+        print("onError:")
+        print("  ws={0}".format(ws))
+        print("  error={0}".format(error))
+
+    def _onOpen(self, ws):
+        print("onOpen:")
+        print("  ws={0}".format(ws))
+
+    def run(self):
+        self._ws.run_forever()
+
+    def send(self, msg):
+        data = json.dumps(msg)
+        print('>> {0}'.format(data))
+        self._ws.send(data)
