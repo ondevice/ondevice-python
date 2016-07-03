3990ea5d4a0c9b73410a8849ea5ce2c299e00ffc
Manuel Reithuber <git@manuel.reithuber.net>
2016-07-03 10:22:28 +0200

    core/connection.py: Added TunnelSocket as base class for Connection and Response

--- core/connection.py
+++ core/connection.py
@@ -1,7 +1,34 @@
 from core import config, sock
 
+import logging
+import threading
 
-class Connection(sock.Socket):
+class TunnelSocket(sock.Socket):
+    """ Base class for Connection and Response """
+    def __init__(self, *args, **kwargs):
+        self._lock = threading.Lock()
+        self._lock.acquire()
+        super().__init__(*args, **kwargs)
+
+    def _onMessage(self, ws, messageData):
+        logging.debug(b'<< '+messageData)
+        return self._cb(messageData)
+
+    def _onOpen(self, ws):
+        self._lock.release()
+        super()._onOpen(ws)
+
+    def send(self, msg):
+        self._lock.acquire()
+        logging.debug(b'>> '+msg)
+        if (type(msg) == bytes):
+            self._ws.send(msg, 2) # OPCODE_BINARY
+        else:
+            self._ws.send(msg, 1) # OPCODE_TEXT
+        self._lock.release()
+
+
+class Connection(TunnelSocket):
     def __init__(self, dev, protocol, service, cb=None, auth=None):
         self._cb = cb
 
@@ -15,25 +42,8 @@ class Connection(sock.Socket):
 
         super().__init__('/connect', auth=auth, dev=dev, protocol=protocol, service=service)
 
-    def _onMessage(self, ws, messageData):
-        return self._cb(messageData)
 
-    def send(self, msg):
-        if (type(msg) == bytes):
-            self._ws.send(msg, 2) # OPCODE_BINARY
-        else:
-            self._ws.send(msg, 1) # OPCODE_TEXT
-
-class Response(sock.Socket):
+class Response(TunnelSocket):
     def __init__(self, broker, tunnelId, dev, cb=None):
         super().__init__('/accept', tunnel=tunnelId, dev=dev)
         self._cb = cb
-
-    def _onMessage(self, ws, messageData):
-        return self._cb(messageData)
-
-    def send(self, msg):
-        if (type(msg) == bytes):
-            self._ws.send(msg, 2) # OPCODE_BINARY
-        else:
-            self._ws.send(msg, 1) # OPCODE_TEXT
