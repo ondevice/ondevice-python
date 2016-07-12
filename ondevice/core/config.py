6359482f0e65aac772f6474d733b8269247c02b0
Manuel Reithuber <git@manuel.reithuber.net>
2016-07-12 16:52:33 +0200

    added new functionality to config.py

--- ondevice/core/config.py
+++ ondevice/core/config.py
@@ -7,14 +7,27 @@ import os
 
 _config = None
 
-def getClientAuth(): return _getValue('client', 'auth')
-def getDeviceAuth(): return _getValue('device', 'auth')
-def getDeviceId(): return _getValue('device', 'id')
+def addSection(name):
+    return _getConfig().add_section(name)
 
-def setClientAuth(auth): _setValue('client', 'auth', auth)
-def setDeviceAuth(auth): _setValue('device', 'auth', auth)
-def setDeviceId(devId): _setValue('device', 'id', devId)
-def setDeviceName(slug): _setValue('device', 'name', slug)
+def getClientAuth(): return getValue('client', 'auth')
+def getDeviceAuth(): return getValue('device', 'auth')
+def getDeviceId(): return getValue('device', 'id')
+
+def hasSection(name):
+    return _getConfig().has_section(name)
+
+def hasValue(section, name):
+    return _getConfig().has_option(section, name)
+
+def listKeys(section):
+    return _getConfig().options(section)
+
+def setClientAuth(auth): setValue('client', 'auth', auth)
+def setDeviceAuth(auth): setValue('device', 'auth', auth)
+def setDeviceId(devId): setValue('device', 'id', devId)
+def setDeviceName(slug): setValue('device', 'name', slug)
+
 
 
 def _getConfig():
@@ -28,6 +41,9 @@ def _getConfig():
         for s in ['client', 'device']:
             if not _config.has_section(s):
                 _config.add_section(s)
+        if not _config.has_section('services'):
+            _config.add_section('services')
+            _config.set('services', 'ssh', '{"protocol": "ssh"}')
 
     return _config
 
@@ -41,14 +57,24 @@ def _getConfigPath(filename):
 
     return os.path.join(configDir, filename)
 
-def _getValue(section, key, default=None):
+def getValue(section, key, default=None):
     cfg = _getConfig()
     if cfg.has_option(section, key):
         return cfg.get(section, key)
     else:
         return default
 
-def _setValue(section, key, value):
+def remove(section, key):
+    cfg = _getConfig()
+    if not cfg.remove_option(section, key):
+        raise KeyError("No such value: {0}/{1}".format(section, key))
+
+    # TODO store a temporary file first (to avoid corrupting the config file in case of a full disk)
+    cfgPath = _getConfigPath('ondevice.conf')
+    with open(cfgPath, 'w') as f:
+        cfg.write(f)
+
+def setValue(section, key, value):
     cfg = _getConfig()
     cfg.set(section, key, value)
 
