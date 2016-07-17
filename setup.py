a0d355bd0201cd1f8edeb6b10015299e123a28ea
Manuel Reithuber <git@manuel.reithuber.net>
2016-07-17 14:44:02 +0200

    v0.1dev7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1dev6',
+    version='0.1dev7',
 
     description='Client for the ondevice.io service',
     long_description=long_description,
