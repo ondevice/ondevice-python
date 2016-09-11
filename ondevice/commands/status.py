7fb11861ba1c4028495dd533de957f81d99a1c05
Manuel Reithuber <git@manuel.reithuber.net>
2016-09-11 12:56:27 +0200

    added `ondevice status` command

---
+++ ondevice/commands/status.py
@@ -0,0 +1,37 @@
+"""
+Prints the client version and ondeviceID (if the daemon is running).
+
+Note that this command will include further information (like the daemon version
+in case we're querying a non-local daemon) in the future.
+
+Returns:
+- 0 if the daemon is running
+- 1 if it isn't
+- >1 on errors (in the future)
+"""
+
+import ondevice
+from ondevice.core import config, daemon
+
+import psutil
+
+usage = {
+    'msg': 'prints information on the ondevice client and daemon',
+}
+
+def run():
+    devId = None
+    if daemon.getDaemonPID() != None:
+        # TODO as soon as the config keys are updated, use sth. like config.getDeviceId()
+        # (right now config.getDeviceId() returns the key the device uses to authenticate
+        # itself - which was a mess up on my side - mreithub)
+        devId = config.getValue('device', 'name')
+
+    if devId != None:
+        print("Device:")
+        print("  ID: {0}".format(devId))
+
+    print("Client:")
+    print("  version: {0}".format(ondevice.getVersion()))
+
+    return 0 if devId != None else 1
