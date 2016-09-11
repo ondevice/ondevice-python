77d0a28998dbc71cdff58069b534af57d05f8dd8
Manuel Reithuber <git@manuel.reithuber.net>
2016-09-11 13:08:44 +0200

    added custom base class for all our exception classes (supports kwargs that will be set as attributes)

--- ondevice/core/exception.py
+++ ondevice/core/exception.py
@@ -1,18 +1,24 @@
 """ Some exception classes for the ondevice client """
 
+class _Exception(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args)
+        self.msg = args[0]
+        for k,v in kwargs.items():
+            setattr(self, k, v)
 
-class ConfigurationError(Exception):
+class ConfigurationError(_Exception):
     """ Indicates a missing/faulty configuration value """
     pass
 
-class ImplementationError(Exception):
+class ImplementationError(_Exception):
     """ Indicates implementation issues with a command or module """
     pass
 
-class TransportError(Exception):
+class TransportError(_Exception):
     """ Indicates a communication error with the server """
     pass
 
-class UsageError(Exception):
-    """ Indicates issues with the commandline usage (unsupported argument, etc.) """
+class UsageError(_Exception):
+    """ Indicates issues with the commandline usage (unknown command, unsupported argument, etc.) """
     pass
