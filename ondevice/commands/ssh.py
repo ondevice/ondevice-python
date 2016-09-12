c238f38ba7f3e4969a38e8cd7e38ae6c599e95af
Manuel Reithuber <git@manuel.reithuber.net>
2016-09-12 23:38:00 +0200

    `ondevice ssh` now parses the ssh options (to be less dependent on ssh argument order - something we need for the upcoming `ondevice rsync`)

--- ondevice/commands/ssh.py
+++ ondevice/commands/ssh.py
@@ -8,8 +8,8 @@ This is a relatively thin wrapper around the `ssh` command.
 The main difference to invoking ssh directly is that instead of regular host names you'll have to specify an ondevice deviceId.
 The connection is routed through the ondevice.io network.
 
-Note that the user@device part has to be the first argument (all arguments
-following it will be passed to ssh).
+ondevice ssh will try to parse ssh's arguments, the first non-argument has to be
+the user@hostname combo.
 
 See ssh's documentation for further details.
 
@@ -31,29 +31,39 @@ Examples:
 
 from ondevice.core import connection
 
+import getopt
 import subprocess
 import sys
 
 usage = {
     'msg': 'Connect to your devices using the ssh protocol',
-    'args': '[<user>@]<device> [ssh-arguments...]',
+    'args': '[ssh-arguments...]',
     'group': 'client'
 }
 
-def run(target, *args):
-    if '@' in target:
-        sshUser, devId = target.split('@', 1)
-    else:
-        sshUser = None
-        devId = target
+def run(*args):
+    # option list copied from debian jessie's openssh source package (from ssh.c, line 509)
+    opts, nonopts = getopt.getopt(args, '1246ab:c:e:fgi:kl:m:no:p:qstvx')
+    nonopts = list(nonopts)
 
+    devId = nonopts.pop(0)
+    sshUser = None
+    if '@' in devId:
+        sshUser, devId = devId.split('@', 1)
 
-    proxyCmd = 'ProxyCommand={0} connect ssh:tunnel {1}'.format(sys.argv[0], devId)
-    if sshUser != None:
-        args = ['-l', sshUser]+list(args)
+    # prepare command
+    cmd = ['ssh', '-oProxyCommand={0} connect ssh:tunnel {1}'.format(sys.argv[0], devId)]
 
-    # this prevents multiple known_hosts entries per device (a unqualified and a qualified one)
+    # add options
+    for k,v in opts:
+        cmd.append('{0}{1}'.format(k,v))
+
+    # add hostname (and user) in the format user@ondevice:devUser.devId
     qualifiedId = connection.qualifyDeviceId(devId)
+    hostname = 'ondevice:{0}'.format(qualifiedId)
+    if sshUser != None:
+        hostname = '{0}@{1}'.format(sshUser, hostname)
+    cmd.append(hostname)
 
-    ssh = subprocess.Popen(['ssh', '-o', proxyCmd, 'ondevice:{0}'.format(qualifiedId)]+list(args), stdin=None, stdout=None, stderr=None)
+    ssh = subprocess.Popen(cmd + nonopts, stdin=None, stdout=None, stderr=None)
     return ssh.wait()
